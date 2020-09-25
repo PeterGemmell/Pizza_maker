e759234efbc57e193d7bbc0a0f685f644001117e
Peter Gemmell <petergemmell65@hotmail.com>
2020-09-25 14:55:45 +0100

    initial html created

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import App from './App.vue'
 var app = new Vue({
   el: "#app",
   data: {
+    showSauce: false,
+    
 
   },
   methods: {
-    
+
   }
 })
+
+// Here we have a new Vue instance called app. The el of #app means that this Vue instance will be bound
+// to a a div with an id of app in our HTML.
